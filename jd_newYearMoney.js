67a7b1cc9f502625ae25935f5bfaf2b62f69a8f5
Aerozb <847888663@qq.com>
2021-02-01 12:35:11 +0800

    FIX

--- jd_newYearMoney.js
+++ jd_newYearMoney.js
@@ -40,6 +40,7 @@ const inviteCodes = [
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
+
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
